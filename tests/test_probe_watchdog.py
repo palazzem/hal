1e97a7b21df86e77b97e000fa97e2cb71a533bc1
Emanuele Palazzetti <hello@palazzetti.me>
2019-11-16 03:44:28 +0100

    [tests] WatchdogProbe tests are now using Mock; marking xfail the integration test

--- tests/test_probe_watchdog.py
+++ tests/test_probe_watchdog.py
@@ -1,3 +1,4 @@
+import pytest
 import logging
 
 from hal.probes.watchdog import WatchdogProbe
@@ -22,32 +23,52 @@ def test_watchdog_run_without_hosts(caplog):
         assert result is False
 
 
-def test_watchdog_success():
+def test_watchdog_success(mocker):
     """Should succeed with valid hosts to monitor."""
+    process = mocker.patch("subprocess.run")
+    process.return_value.returncode = 0
     probe = WatchdogProbe({"hosts": ["127.0.0.1"]})
     probe.run()
     assert len(probe.results) == 1
     assert probe.results["hal.watchdog.detected_hosts"] == 1
 
 
-def test_watchdog_multiple_hosts():
+def test_watchdog_multiple_hosts(mocker):
     """Should detect multiple hosts."""
+    process = mocker.patch("subprocess.run")
+    process.return_value.returncode = 0
     probe = WatchdogProbe({"hosts": ["127.0.0.1", "127.0.0.1"]})
     probe.run()
     assert len(probe.results) == 1
     assert probe.results["hal.watchdog.detected_hosts"] == 2
 
 
-def test_watchdog_partial_failure():
+def test_watchdog_partial_failure(mocker):
     """Should report only detected hosts."""
+    process = mocker.patch("subprocess.run")
+    type(process.return_value).returncode = mocker.PropertyMock(side_effect=[0, 1])
     probe = WatchdogProbe({"hosts": ["127.0.0.1", "invalid-host"]})
     probe.run()
     assert len(probe.results) == 1
     assert probe.results["hal.watchdog.detected_hosts"] == 1
 
 
-def test_watchdog_failure(caplog):
+@pytest.mark.xfail
+def test_watchdog_partial_failure_integration(mocker):
+    """Should report only detected hosts. This test runs the `ping` command,
+    but in some CI it can fail to prevent DoS. Because of that, the test
+    is marked as it can fail.
+    """
+    probe = WatchdogProbe({"hosts": ["127.0.0.1", "invalid-host"]})
+    probe.run()
+    assert len(probe.results) == 1
+    assert probe.results["hal.watchdog.detected_hosts"] == 1
+
+
+def test_watchdog_failure(caplog, mocker):
     """Should log a debug entry if it fails."""
+    process = mocker.patch("subprocess.run")
+    process.return_value.returncode = 1
     probe = WatchdogProbe({"hosts": ["invalid-host"]})
     with caplog.at_level(logging.DEBUG):
         result = probe.run()
