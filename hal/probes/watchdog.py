96264091acf81a1c8309579a98fc900313fe1cb4
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2019-12-14 19:55:45 +0100

    [component/probe] Watchdog probe metrics are tagged by name

--- hal/probes/watchdog.py
+++ hal/probes/watchdog.py
@@ -9,9 +9,9 @@ log = logging.getLogger(__name__)
 
 class WatchdogProbe(BaseProbe):
     """WatchdogProbe Probe detects if a list of hosts are connected to the
-    probe network. This probe requires a list of hosts address:
-        * ["127.0.0.1"]
-        * ["192.168.1.1", "test-server"]
+    probe network. This probe requires a list of hosts tuples (address, tag-name):
+        * [("127.0.0.1", "palazzem")]
+        * [("192.168.1.1", "palazzem"), ("test-server", "tali")]
 
     Under the hood, `WatchdogProbe` uses `ping` sending a single packet and
     checking the return code. `subprocess` is used
@@ -28,16 +28,20 @@ class WatchdogProbe(BaseProbe):
             # Bail out if hosts are not defined
             return False, "run failed for missing hosts to monitor"
 
-        # Metric: number of detected hosts
-        self.results["hal.watchdog.detected_hosts"] = 0
+        # Dict used to aggregate results instead of extra iterations
+        detected_hosts = {}
 
         for host in self.config["hosts"]:
             # Ping the host to check if present in the network
-            process = subprocess.run(["ping", "-c", "1", host], capture_output=True)
+            address, name = host
+            process = subprocess.run(["ping", "-c", "1", address], capture_output=True)
             if process.returncode == 0:
-                self.results["hal.watchdog.detected_hosts"] += 1
+                check = detected_hosts.get(name) or (0, [name])
+                detected_hosts[name] = (check[0] + 1, check[1])
             else:
                 # Keep debug information with `ping` stdout
-                log.debug("Probe watchdog: host '%s' not found", host)
+                log.debug("Probe watchdog: host '%s' not found", address)
 
+        # Metric: number of detected hosts by tag (name)
+        self.results["hal.watchdog.detected_hosts"] = list(detected_hosts.values())
         return True, None
